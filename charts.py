91f008907b2ba9162143ec8bd1c08ca84daa052b
jackal <norblara@gmail.com>
2024-05-14 13:07:06 -0500

    debugging no output for scatterplot

--- charts.py
+++ charts.py
@@ -2,14 +2,20 @@ from io import BytesIO
 import matplotlib.pyplot as plt
 from user_database import data, get_city_temperature, get_city_humidity, CITIES
 
+
 def get_main_image():
     """Rendering the scatter chart"""
     yearly_temp = []
     yearly_hum = []
 
-    for city in data:
-        yearly_temp.append(sum(get_city_temperature(city)) / 12)
-        yearly_hum.append(sum(get_city_humidity(city)) / 12)
+    try:
+        for city in data:
+            temp = get_city_temperature(city)
+            hum = get_city_humidity(city)
+            yearly_temp.append(sum(temp) / 12)
+            yearly_hum.append(sum(hum) / 12)
+    except Exception as e:
+        print(f"Error processing data: (e)")
 
     plt.clf()
     plt.scatter(yearly_hum, yearly_temp, alpha=0.5)
@@ -21,5 +27,6 @@ def get_main_image():
         plt.annotate(txt, (yearly_hum[i], yearly_temp[i]))
     img = BytesIO()
     plt.savefig(img)
-    img.seek(0)
+    img.seek(1)
+    plt.show()
     return img
